0d48d3248dd4b3550439bd3d406345d67a574fa8
ezzabuzaid <Ezzabuzaid@hotmail.com>
2022-01-16 18:46:58 +0200

    feat: export RootServiceCollection as public API

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import RootServiceCollection from './RootServiceCollection';
+
 export * from './AbstractServiceCollection';
 export * from './Context';
 export * from './Exceptions';
@@ -6,6 +8,8 @@ export * from './Inject';
 export * from './Injectable';
 export * from './InjectionToken';
 export * from './Injector';
+export * from './ServiceCollection';
 export * from './ServiceLifetime';
-
 export { ServiceType } from './Types';
+export { RootServiceCollection };
+
